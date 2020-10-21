1eeb64f4917539482fb4e79a7383d9c7d5d984b3
FiendChain <formulating.response@gmail.com>
2020-10-21 16:56:22 +1100

    Automated kuzu logging

--- kuzu.py
+++ kuzu.py
@@ -44,8 +44,9 @@ class NetFull(nn.Module):
 class NetConv(nn.Module):
     # two convolutional layers and one fully connected layer,
     # all using relu, followed by log_softmax
-    def __init__(self):
+    def __init__(self, activation=F.relu):
         super(NetConv, self).__init__()
+        self.activation = activation
 
         conv1_ksize = 3
         conv1_stride = 1
@@ -70,18 +71,75 @@ class NetConv(nn.Module):
 
     def forward(self, x):
         x = self.conv1(x)
-        x = F.relu(x)
+        x = self.activation(x)
 
         x = self.conv2(x)
-        x = F.relu(x)
+        x = self.activation(x)
         x = self.maxpool1(x)
         x = self.dropout1(x)
 
         x = torch.flatten(x, 1)
 
         x = self.fc1(x)
-        x = F.relu(x)
-        x = self.dropout1(x)
+        x = self.activation(x)
+        x = self.dropout2(x)
+
+        x = self.fc2(x)
+        output = F.log_softmax(x, dim=1)
+        return output
+
+class NetConvMini(nn.Module):
+    # two convolutional layers and one fully connected layer,
+    # all using relu, followed by log_softmax
+    def __init__(self, activation=F.relu, dense_size=128, use_dropout=True):
+        super(NetConvMini, self).__init__()
+        self.use_dropout = use_dropout
+        self.activation = activation
+        self.dense_size = dense_size
+
+        conv1_ksize = 5
+        conv1_stride = 1
+        conv1_osize = math.ceil((KUZU_SIZE[0]-conv1_ksize)/conv1_stride) + 1
+
+        max_pool_1 = 2
+        max_pool_1_size = math.floor(conv1_osize/max_pool_1) 
+
+        conv2_ksize = 5
+        conv2_stride = 1
+        conv2_osize = math.ceil((max_pool_1_size-conv2_ksize)/conv2_stride + 1)
+
+        max_pool_2 = 2
+        max_pool_2_size = math.floor(conv2_osize/max_pool_2) 
+
+        fc1_size = max_pool_2_size*max_pool_2_size*64
+
+        self.conv1 = nn.Conv2d(1, 32, conv1_ksize, conv1_stride)
+        self.conv2 = nn.Conv2d(32, 64, conv2_ksize, conv2_stride)
+        self.dropout1 = nn.Dropout2d(0.5)
+        self.dropout2 = nn.Dropout2d(0.5)
+        self.maxpool1 = nn.MaxPool2d(max_pool_1)
+        self.maxpool2 = nn.MaxPool2d(max_pool_2)
+        self.fc1 = nn.Linear(fc1_size, dense_size)
+        self.fc2 = nn.Linear(dense_size, KUZU_TARGETS)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.activation(x)
+        x = self.maxpool1(x)
+
+        x = self.conv2(x)
+        x = self.activation(x)
+        x = self.maxpool2(x)
+
+        if self.use_dropout:
+            x = self.dropout1(x)
+
+        x = torch.flatten(x, 1)
+
+        x = self.fc1(x)
+        x = self.activation(x)
+        if self.use_dropout:
+            x = self.dropout2(x)
 
         x = self.fc2(x)
         output = F.log_softmax(x, dim=1)
--- kuzu_main.py
+++ kuzu_main.py
@@ -17,9 +17,10 @@ import pandas as pd
 from torchvision import datasets, transforms
 from torchsummary import summary
 
-from kuzu import NetLin, NetFull, NetConv
+from kuzu import NetLin, NetFull, NetConv, NetConvMini
 
 import os
+import pickle
 
 def train(args, model, device, train_loader, optimizer, epoch):
     model.train()
@@ -66,18 +67,33 @@ def test(args, model, device, test_loader):
 
     accuracy = correct / len(test_loader.dataset)
 
-    return (conf_matrix, accuracy) 
+    return (conf_matrix, accuracy, test_loss) 
+
+ACTIVATIONS = {
+    "relu": torch.nn.functional.relu,
+    "tanh": torch.nn.functional.tanh,
+    "sigmoid": torch.nn.functional.sigmoid,
+    "elu": torch.nn.ELU(),
+    "selu": torch.nn.SELU(),
+    "lelu": torch.nn.LeakyReLU()
+}
+
+ACTIVATION_KEYS = ','.join(ACTIVATIONS.keys())
 
 def main():
     # command-line arguments
     parser = argparse.ArgumentParser()
-    parser.add_argument('--net',type=str,default='full',help='lin, full or conv')
+    parser.add_argument('--net',type=str,default='full',help='lin, full, conv or conv_mini')
+    parser.add_argument("--activation", type=str, default="relu", help=ACTIVATION_KEYS)
+    parser.add_argument("--dense", type=int, default=128, help="number of dense layers in conv or conv_mini")
+    parser.add_argument("--dropout", action="store_true", default=False, help="Apply dropout")
     parser.add_argument('--lr',type=float,default=0.01,help='learning rate')
     parser.add_argument('--mom',type=float,default=0.5,help='momentum')
     parser.add_argument('--epochs',type=int,default=10,help='number of training epochs')
     parser.add_argument('--no_cuda',action='store_true',default=False,help='disables CUDA')
     parser.add_argument('--save',action='store_true',default=False,help='enable saving')
     parser.add_argument('--infer',action="store_true",default=False,help='infer only')
+    parser.add_argument('--override', action='store_true', default=False, help="override existing save")
     args = parser.parse_args()
 
     use_cuda = not args.no_cuda and torch.cuda.is_available()
@@ -98,31 +114,67 @@ def main():
     testset = datasets.KMNIST(root='./data', train=False, download=True, transform=transform)
     test_loader = torch.utils.data.DataLoader(testset, batch_size=64, shuffle=False)
 
+    if args.activation in ACTIVATIONS:
+        activation = ACTIVATIONS[args.activation]
+    else:
+        print(f"error: unknown activation function '{args.activation}'")
+        return 1
+
+    if args.dense < 0:
+        print(f"error: negative number of dense neurons '{args.dense}'")
+        return 1
+
+    use_dropout_str = "dropout" if args.dropout else "no_dropout"
+
     # choose network architecture
     if args.net == 'lin':
         net = NetLin().to(device)
+        model_prefix = 'lin'
     elif args.net == 'full':
         net = NetFull().to(device)
+        model_prefix = 'full'
     elif args.net == 'conv':
-        net = NetConv().to(device)
+        net = NetConv(activation=activation).to(device)
+        model_prefix = f"conv_{args.activation}"
+    elif args.net == 'conv_mini':
+        net = NetConvMini(activation=activation, dense_size=args.dense, use_dropout=args.dropout).to(device)
+        model_prefix = f"conv_mini_{args.activation}_{args.dense}_{use_dropout_str}"
     else:
         print(f"error: unknown net type '{args.net}'")
         return 1
 
+    print(f"info: creating network {model_prefix}")
+
     summary(net, (1,28,28))
     if not args.save:
         print('warning: saving disabled')
+    
 
-    net_fname = f"kmnist_{args.net}.pt"
-    if os.path.exists(net_fname):
-        net.load_state_dict(torch.load(net_fname))
+    net_fname = f"kmnist_{model_prefix}.pt"
+    log_fname = f"kmnist_{model_prefix}.log"
+
+    if args.override:
+        opt = input(f"overriding {net_fname} and {log_fname}. Are you sure? (y/n)")
+        if opt.lower() != 'y':
+            return 1
+
+    logs = []
+
+    if not args.override:
+        if os.path.exists(log_fname):
+            with open(log_fname, "rb") as fp:
+                logs = pickle.load(fp)
+                print(f"Resuming from log of size={len(logs)}")
+
+        if os.path.exists(net_fname):
+            net.load_state_dict(torch.load(net_fname))
 
     if not list(net.parameters()):
         print(f"error: model {args.net} has no parameters")
         return 1
 
     if args.infer:
-        conf_matrix, acc = test(args, net, device, test_loader)
+        conf_matrix, acc, _ = test(args, net, device, test_loader)
 
         labels = "o,ki,su,tsu,na,ha,ma,ya,re,wo".split(",")
         df_cm = pd.DataFrame(conf_matrix, index=labels, columns=labels)
@@ -136,19 +188,27 @@ def main():
 
         return 0
 
+    last_epoch = len(logs)+1
+
     # training and testing loop
     try:
         # use SGD optimizer
         optimizer = optim.SGD(net.parameters(), lr=args.lr, momentum=args.mom)
-        for epoch in range(1, args.epochs + 1):
+        for epoch in range(last_epoch, args.epochs + last_epoch):
             train(args, net, device, train_loader, optimizer, epoch)
-            test(args, net, device, test_loader)
+            train_log = test(args, net, device, train_loader)
+            test_log = test(args, net, device, test_loader)
+            logs.append((train_log, test_log))
+
     except KeyboardInterrupt:
         pass
     finally:
         if args.save:
             print(f"info: saving model {net_fname}")
             torch.save(net.state_dict(), net_fname)
+            with open(log_fname, "wb+") as fp:
+                pickle.dump(logs, fp)
+                print(f"saving logs size={len(logs)}")
     
     return 0
         
